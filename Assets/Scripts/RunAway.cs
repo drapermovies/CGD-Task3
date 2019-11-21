f6d700f8544418d543dce669dec0bc4261e4ed06
Drew <Drew2.Mayhew@live.uwe.ac.uk>
2019-11-21 01:12:02 +0000

    NavMesh + Move Away Script

---
+++ Assets/Scripts/RunAway.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class RunAway : MonoBehaviour
+{
+    private NavMeshAgent _agent;
+
+    public GameObject Player;
+
+    public float EnemyDistanceRun = 4.0f;
+
+
+    void Start()
+    {
+        _agent = GetComponent<NavMeshAgent>();
+    }
+
+
+    void Update()
+    {
+        float distance = Vector3.Distance(transform.position, Player.transform.position);
+        Debug.Log("Distance: " + distance);
+
+        //Run Away From Player
+        if (distance < EnemyDistanceRun)
+        {
+            //Vector Player To Enemy
+            Vector3 dirToPlayer = transform.position - Player.transform.position;
+
+            Vector3 newPos = transform.position + dirToPlayer;
+
+            _agent.SetDestination(newPos);
+        }
+
+    }
+}
