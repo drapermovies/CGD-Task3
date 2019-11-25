2d5ecb0dba7700f2701d70a0a64a4d9e79a571d9
Quill <tylerw1997@hotmail.co.uk>
2019-11-25 07:16:54 +0000

    started a menu, gonna work on camera when switching between sub menus next

---
+++ Assets/Scripts/AnimatorFunctions.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimatorFunctions : MonoBehaviour
+{
+    [SerializeField] MenuController menuController;
+    public bool disableOnce;
+
+    void PlaySound(AudioClip whichSound)
+    {
+        if (!disableOnce)
+        {
+            menuController.audioSource.PlayOneShot(whichSound);
+        }
+        else
+        {
+            disableOnce = false;
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/MenuButton.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuButton : MonoBehaviour
+{
+
+    [SerializeField] MenuController menuController;
+    [SerializeField] Animator animator;
+    [SerializeField] AnimatorFunctions animatorFunctions;
+    [SerializeField] int thisIndex;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        universalAnimation();
+        individualAnimation();
+    }
+
+    private void universalAnimation()
+    {
+        //transition buttons in
+        if (menuController.hasTransitionedIn == false)
+        {
+            animator.SetBool("transitionedIn", true);
+        }
+
+        //transition buttons out
+        if (menuController.hasTransitionedOut == true)
+        {
+            animator.SetBool("transitionedOut", false);
+        }
+    }
+
+    private void individualAnimation()
+    {
+        if (menuController.index == thisIndex)
+        {
+            animator.SetBool("selected", true);
+            if (Input.GetAxis("Submit") == 1)
+            {
+                animator.SetBool("pressed", true);
+            }
+            else if (animator.GetBool("pressed"))
+            {
+                animator.SetBool("pressed", false);
+                animatorFunctions.disableOnce = true;
+                menuController.hasTransitionedOut = true;
+            }
+        }
+        else
+        {
+            animator.SetBool("selected", false);
+        }
+    }
+}
