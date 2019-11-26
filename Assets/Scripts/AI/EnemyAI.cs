6911b0f9f5ecb2204d21a1b6f87cd06e60573bcb
Joel Draper <jepdraper@gmail.com>
2019-11-26 19:25:38 +0000

    Made the enemy code work better
    Removed bump object script functionality
    Added bump object to enemy script
    Added waypoint pathfinding to enemy script
    Updated Enemy prefab

--- Assets/Scripts/AI/EnemyAI.cs
+++ Assets/Scripts/AI/EnemyAI.cs
@@ -1,5 +1,7 @@
-﻿using UnityEngine;
-using System.Collections;
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
 using UnityEngine.AI;
 
 public class EnemyAI : MonoBehaviour
@@ -12,8 +14,11 @@ public class EnemyAI : MonoBehaviour
     public Rigidbody rb;
     public float vSpeed;
 
+    public List<Vector3> waypoints = new List<Vector3>();
+
     public bool has_bumped { get; set; }
 
+    [SerializeField] private float bump_range = 0.5f;
     [SerializeField] private Transform target_marker = null;
     [SerializeField] private ParticleSystem run_particles = null;
     [SerializeField] private ParticleSystem bump_particles = null;
@@ -21,6 +26,8 @@ public class EnemyAI : MonoBehaviour
     private Animator anim;
 
     private NavMeshAgent[] nav_agents;
+
+    private float new_bump = 0.0f;
 
     void Awake()
     {
@@ -54,6 +61,7 @@ public class EnemyAI : MonoBehaviour
     void Update()
     {
         CheckIdleState();
+        BumpCheck();
 
         if (Input.GetKeyDown("space") && anim.GetBool("isIdle"))
         {
@@ -122,6 +130,11 @@ public class EnemyAI : MonoBehaviour
         //Line at the mouse position
         Debug.DrawLine(target_marker.position,
                        target_marker.position + Vector3.up * 5);
+
+        foreach(Vector3 point in waypoints)
+        {
+            Gizmos.DrawCube(point, new Vector3(0.5f, 0.5f, 0.5f));
+        }
     }
 
     public void UpdateTargets(Vector3 target_pos)
@@ -162,6 +175,10 @@ public class EnemyAI : MonoBehaviour
 
         run_particles.Clear();
         run_particles.Stop();
+
+        FindPlayer();
+
+        FindNearestWaypoint(); //Go to nearest point if idle
     }
 
     public void Run()
@@ -172,6 +189,7 @@ public class EnemyAI : MonoBehaviour
         GetComponent<NavMeshAgent>().speed = 3.5f; //Move faster
     }
 
+    //Plays a particle when we 'bump' into something
     public void Bump()
     {
         if (!has_bumped)
@@ -179,5 +197,95 @@ public class EnemyAI : MonoBehaviour
             bump_particles.Play();
             has_bumped = true;
         }
+    }
+
+    //Randomly generates a bump particle if we're too close to an object
+    private void BumpCheck()
+    {
+        float nearest_obj = 9999.99f;
+        float distance = 0.0f;
+
+        //Nearest nav mesh obj.
+        foreach (NavMeshObstacle obj in FindObjectsOfType<NavMeshObstacle>())
+        {
+            distance = Vector3.Distance(this.transform.position, obj.transform.position);
+            if (distance < nearest_obj)
+            {
+                nearest_obj = distance;
+                ChangeDistance(obj.transform.localScale);
+                if (Vector3.Distance(this.transform.position, obj.transform.position) <= new_bump)
+                {
+                    Bump();
+                }
+                else
+                {
+                    has_bumped = false;
+                }
+            }
+        }
+    }
+
+    //Changes bump distance in relation to obj scale
+    private void ChangeDistance(Vector3 scale)
+    {
+        float largest_value = 0;
+        if (scale.x > largest_value)
+        {
+            largest_value = transform.localScale.x;
+        }
+        if (scale.y > largest_value)
+        {
+            largest_value = transform.localScale.y;
+        }
+        if (scale.z > largest_value)
+        {
+            largest_value = transform.localScale.z;
+        }
+
+        new_bump = bump_range + largest_value;
+    }
+
+    //Go to nearest 'safe' space
+    private void FindNearestWaypoint()
+    {
+        float max_distance = 9999.9f;
+        Vector3 nearest_point = Vector3.zero;
+        foreach(Vector3 point in waypoints)
+        {
+            float distance = Vector3.Distance(transform.position, point);
+
+            if (distance < max_distance)
+            {
+                max_distance = distance;
+                nearest_point = point;
+            }
+        }
+
+        //We're already here, so find somewhere else to go
+        if(Vector3.Distance(nearest_point, transform.position) < bump_range)
+        {
+            int random = Random.Range(0, waypoints.Count);
+            nearest_point = waypoints[random]; 
+        }
+
+        UpdateTargets(nearest_point);
+    }
+
+    private void FindPlayer()
+    {
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
+
+        if (player != null)
+        {
+            Vector3 player_pos = player.transform.position;
+
+            //If the player is too close, we run away
+            if (Vector3.Distance(transform.position, player_pos) <= 100)
+            {
+                Run();
+                return;
+            }
+        }
+        CrippledWalk();
     }
 }
