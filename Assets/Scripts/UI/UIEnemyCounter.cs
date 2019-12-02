e25c5ae818918b21478e10cf9f33c64a5e8afee6
Tom <Thomas2.Paradise@live.uwe.ac.uk>
2019-12-02 17:38:56 +0000

    ui work

--- Assets/Scripts/UI/UIEnemyCounter.cs
+++ Assets/Scripts/UI/UIEnemyCounter.cs
@@ -8,16 +8,31 @@ public class UIEnemyCounter : MonoBehaviour
     private Text counterText;
     private int counter;
     private string startText;
+
+    private RectTransform rectTransform;
+    private Vector2 pos = new Vector3(0, -0.15f);
+    private Vector2 velocity = Vector2.zero;
+
     // Start is called before the first frame update
     void Start()
     {
         counterText = gameObject.GetComponent<Text>();
         startText = counterText.text;
+        rectTransform = GetComponent<RectTransform>();
+        counterText.text += "0/5";
+    }
+
+    private void Update()
+    {
+        if (rectTransform.anchoredPosition != pos)
+        {
+            rectTransform.anchoredPosition = Vector2.SmoothDamp(rectTransform.anchoredPosition, pos, ref velocity, 0.7f);
+        }
     }
 
     public void updateCounter()
     {
         counter--;
-        counterText.text = startText + counter;
+        counterText.text = startText + counter + "/5";
     }
 }
--- Assets/Scripts/UI/UITimer.cs
+++ Assets/Scripts/UI/UITimer.cs
@@ -6,26 +6,45 @@ using UnityEngine.UI;
 public class UITimer : MonoBehaviour
 {
     private Text timerText;
+    public GameObject enemyCounter;
     public float timer;
     private string startText;
+
+    private RectTransform rectTransform;
+    private Vector2 pos = new Vector3(0, 0.15f);
+    private Vector2 velocity = Vector2.zero;
+
     // Start is called before the first frame update
     void Start()
     {
         timerText = gameObject.GetComponent<Text>();
         startText = timerText.text;
         timer *= 60;
+        rectTransform = GetComponent<RectTransform>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        timer -= Time.deltaTime;
+        if (rectTransform.anchoredPosition != pos)
+        {
+            rectTransform.anchoredPosition = Vector2.SmoothDamp(rectTransform.anchoredPosition, pos, ref velocity, 0.7f);
+        }
+        else
+        {
+            timer -= Time.deltaTime;
 
-        var minutes = timer / 60; //Divide the guiTime by sixty to get the minutes.
-        var seconds = timer % 60;//Use the euclidean division for the seconds.
-        var fraction = (timer * 100) % 100;
+            if(timer <= 0.0f)
+            {
+                //failure state
+            }
 
-        //update the label value
-        timerText.text = startText + string.Format("{0:00} : {1:00} : {2:000}", minutes, seconds, fraction);
+            var minutes = timer / 60; //Divide the guiTime by sixty to get the minutes.
+            var seconds = timer % 60;//Use the euclidean division for the seconds.
+            var fraction = (timer * 100) % 100;
+
+            //update the label value
+            timerText.text = startText + string.Format("{0:00}:{1:00}:{2:00}", minutes, seconds, fraction);
+        }
     }
 }
