886af1905f76415e531a63e0a93f98a9598a9539
Tom Paradise <thomas2.paradise@live.uwe.ac.uk>
2019-12-03 17:02:07 +0000

    added pulse effect to timer when it reaches 15 secs

---
+++ Assets/Scripts/UI/moveText.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moveText : MonoBehaviour
+{
+    private RectTransform rectTransform;
+    private Vector2 pos = new Vector3(0, 0.15f);
+    private Vector2 velocity = Vector2.zero;
+    private bool inPosition = false;
+    public GameObject image;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rectTransform = GetComponent<RectTransform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (rectTransform.anchoredPosition != pos)
+        {
+            rectTransform.anchoredPosition = Vector2.SmoothDamp(rectTransform.anchoredPosition, pos, ref velocity, 0.7f);
+        }
+        else
+        {
+            if (!inPosition)
+            {
+                inPosition = true;
+                GetComponentInChildren<UITimer>().countDown = true;
+                Destroy(image);
+            }
+        }
+    }
+}
